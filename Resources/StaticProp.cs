51dadc40b38b1c2d74491b7dfd880ae59c3bf2e4
IoanFirst <31549414+Feofan777@users.noreply.github.com>
2019-09-03 18:37:50 +0200

    Working with StartButton, it should create Folders with Monthes names not Monthes integer, get values from dictionary in StaticPop.cs

--- Resources/StaticProp.cs
+++ Resources/StaticProp.cs
@@ -16,6 +16,28 @@ namespace FotoSortIva02.Resources
         // 
         public static string initTextBoxScanFolder = "empty";
         public static string initTextBoxNewFolder = "empty";
+
+        public static string January = "January";
+        public static string February = "February";
+        public static string March = "March";
+        public static string April = "April";
+        public static string May = "May";
+        public static string June = "June";
+        public static string July = "July";
+        public static string August = "August";
+        public static string September = "September";
+        public static string October = "October";
+        public static string November = "November";
+        public static string December = "December";
+
+        public static System.Collections.Generic.Dictionary<int, string> Monthes = new System.Collections.Generic.Dictionary<int, string>
+        {
+            {1, January },{2, February }, {3, March },{4, April },{5, May },{6, June },{7, July },{8, August },{9, September },
+            {10, October },{11, November },{12, December }
+
+        };
+        
+
     }
 
        
--- ViewModel/ViewModelBase.cs
+++ ViewModel/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using FotoSortIva02.Model;
+﻿using ExifLib;
+using FotoSortIva02.Model;
 using FotoSortIva02.Resources;
 using System;
 using System.Collections.Generic;
@@ -16,8 +17,20 @@ namespace FotoSortIva02.ViewModel
     public class ViewModelBase : TextBoxesModel
     {
         #region CTOR
+        string filePath;
         public ViewModelBase()
         {
+            // Delete the log file from the beggining of the Test!
+            string FilePath = String.Format("LOOGGER.txt");
+            filePath = FilePath;
+            // read all lines from txt file and put it to List<string>
+            List<string> lines = File.ReadAllLines(filePath).ToList();
+            List<string> EmptyList = new List<string> { };
+            // make lines to be empty
+            lines = EmptyList;
+            // write to LOG
+            File.WriteAllLines(filePath, lines);
+
             TextBoxGenShow = "Initial Value";
             TextBoxNewFolder = StaticProp.initTextBoxNewFolder;
             TextBoxScanFolder = StaticProp.initTextBoxScanFolder;
@@ -38,8 +51,88 @@ namespace FotoSortIva02.ViewModel
 
         void StartButtAction()
         {
+            // todo change belo line back
             if (StaticProp.ScanningFolderPath != "empty" && StaticProp.CreateFolderPath != "empty")
+            //if (StaticProp.ScanningFolderPath != "empty")
             {
+
+                Pathes pathing = new Pathes(StaticProp.ScanningFolderPath);
+
+                String searchFolder = pathing.InitialFolderPath;
+                var filters = new String[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp", "svg" };
+                filesNames = GetLongFilesFrom(searchFolder, filters, false);
+                foreach (string item in filesNames)
+                {
+                    try
+                    {
+                        using (ExifReader reader = new ExifReader(item))
+                        {
+                            // Extract the tag data using the ExifTags enumeration
+                            DateTime datePictureTaken;
+                            if (reader.GetTagValue<DateTime>(ExifTags.DateTimeDigitized, out datePictureTaken))
+                            {
+                                // Do whatever is required with the extracted information
+                                string messageToWriteSuccess = "The picture was taken on  " +datePictureTaken.ToString();
+
+
+                                //Logger
+                                // read all lines from txt file and put it to List<string>
+                                List<string> lines = File.ReadAllLines(filePath).ToList();
+                                // Add time
+                                lines.Add(DateTime.UtcNow.ToString());
+                                // message
+                                lines.Add(messageToWriteSuccess);
+
+                                File.WriteAllLines(filePath, lines);
+
+                                string monthOfPic = datePictureTaken.Month.ToString();
+                                string yearOfPic = datePictureTaken.Year.ToString();
+                                // making extended Pathes
+                                string extendedYearPath = StaticProp.CreateFolderPath + string.Format("\\{0}", yearOfPic);
+                                string extendedMonthPath = extendedYearPath + string.Format("\\{0}", monthOfPic);
+                                //---------------------
+
+
+                                // check if such a year exist in the new folder
+                                if (File.Exists(extendedYearPath))
+                                {
+                                    //check if such a month exist in the new folder
+                                    if (File.Exists(extendedMonthPath))
+                                    {
+                                        
+
+                                    }
+                                }
+                                else
+                                {
+                                    Directory.CreateDirectory(extendedYearPath);
+                                    Directory.CreateDirectory(extendedMonthPath);
+                                }
+
+
+
+                            }
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        string messageToWriteFailed = "Exception happen" + e.ToString();
+
+                        //Logger
+                        // read all lines from txt file and put it to List<string>
+                        List<string> lines = File.ReadAllLines(filePath).ToList();
+                        // Add time
+                        lines.Add(DateTime.UtcNow.ToString());
+                        // message
+                        lines.Add(messageToWriteFailed);
+
+                        File.WriteAllLines(filePath, lines);
+                       
+
+                    }
+                }
+                
+
 
             }
             else
@@ -50,6 +143,32 @@ namespace FotoSortIva02.ViewModel
             }
         }
         #endregion
+
+        #region Button LogButtCommand
+
+        private ICommand _logButtCommand;
+        public ICommand LogButtCommand
+        {
+            get
+            {
+                return _logButtCommand ?? (_logButtCommand = new CommandHandler(() => LogButtAction(), () => CanExecute));
+            }
+        }
+
+        void LogButtAction()
+        {
+            // create the path
+            var path = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase);
+            //Note that the value in path will be in the form of ..file:\c:\path\to\bin\folder, so before
+            //using the path you may need to strip the file:\ off the front.
+            path = path.Substring(6);
+
+            Process.Start(path);
+        }
+        #endregion
+
+
+
 
         #region Button CreateButtCommand
         private ICommand _createButtCommand;
@@ -115,7 +234,7 @@ namespace FotoSortIva02.ViewModel
 
                 String searchFolder = pathing.InitialFolderPath;
                 var filters = new String[] { "jpg", "jpeg", "png", "gif", "tiff", "bmp", "svg" };
-                filesNames = GetFilesFrom(searchFolder, filters, false);
+                filesNames = GetShortFilesFrom(searchFolder, filters, false);
                 TextBoxFotoCounter=TextBoxFotoCounterMethod(filesNames);
                 TextGenShowMethod(string.Join("\r\n", filesNames));
             }
@@ -124,7 +243,7 @@ namespace FotoSortIva02.ViewModel
 
         // method to return all needed files in folder
 
-        string[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+        string[] GetShortFilesFrom(String searchFolder, String[] filters, bool isRecursive)
         {
             List<String> filesFound = new List<String>();
             //Variant 1 search only in choosen folder
@@ -142,7 +261,7 @@ namespace FotoSortIva02.ViewModel
                 // Variant 1 -------------------- end
 
                 //Variant 2: this getting only the name of the file itself without pathes
-                string[] filename = Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption);
+                string[] filename = System.IO.Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption);
                 List<string> intermediate = new List<string>();
                 foreach (var str in filename)
                 {
@@ -154,6 +273,28 @@ namespace FotoSortIva02.ViewModel
             }
             return filesFound.ToArray();
         }
+
+        string[] GetLongFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+        {
+            List<String> filesFound = new List<String>();
+            //Variant 1 search only in choosen folder
+            //var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            // Variant 1 -------------------- end
+
+            //Variant 2 search in all subfolders
+            var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.AllDirectories;
+            // Variant 2 -------------------- end
+
+            foreach (var filter in filters)
+            {
+                //Variant 1 this getting the full name of the files with the pathes
+                filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+                // Variant 1 -------------------- end
+            }
+            return filesFound.ToArray();
+        }
+
+
         #endregion
         void TextGenShowMethod(string income)
         {
