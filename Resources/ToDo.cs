3ace47ec3d8abef4ea383c6e090b8bfb7fa80360
IoanFirst <31549414+Feofan777@users.noreply.github.com>
2019-09-10 16:13:39 +0200

    dded comments in class Resources>ToDO

---
+++ Resources/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoSortIva02.Resources
+{
+    class ToDo
+    {
+        //to implement:
+        //    - Sort mp4 files as well
+        //    - add checkbox to delete files from scanning folder
+        //    -to add exception during the copy if file exists, copy it with text ( copy1, copy2 etc.)
+
+    }
+}
