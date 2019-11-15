9bbdec35064bba3075529c0965efd8813c3a0131
IoanFirst <31549414+Feofan777@users.noreply.github.com>
2019-11-15 16:06:00 +0100

    i have added some comments to work into ToDo.cs

--- Resources/ToDo.cs
+++ Resources/ToDo.cs
@@ -26,10 +26,11 @@ namespace FotoSortIva02.Resources
         //Y  -to add exception during the copy if file exists, copy it with text ( copy1, copy2 etc.)
         //Y  -block Exit during process
 
-        // ProgressBar is not checked fully
-             // Delete after copying for pic files does not work because Exception "The process cannot access the file because it is being used by another process"
-             // I added in CopyMethods > Move method > functionality of repeat tryials in case of exception
-             // also im trying to find which process holding the file and make this process iDisposable through: using(){}
+        // ProgressBar is not checked fully - when i copy video it seems not changing the progressbar value > can try to ref. the progressbar value to copy video class
+        // Delete after copying for pic files does not work because Exception "The process cannot access the file because it is being used by another process"
+        // I added in CopyMethods > Move method > functionality of repeat tryials in case of exception - doesnt help
+        // also im trying to find which process holding the file and make this process iDisposable through: using(){} - doesnt help
+        // TRY: i think the exif library is holding the files. i can read all exif data before then i can serialize itto some file and then a can startt separate thread to move files.
 
         //Y -Make Text box Status -not changeble IsReadOnly="True" 
         //Y -Make possible to move windows after deleting top panel - now it is possible to drag by the TextBlock Text="Foto Sorting App"
