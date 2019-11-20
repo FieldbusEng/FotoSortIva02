6240f13c2fa42a41e121865e5540853d9e8638ef
IoanFirst <31549414+Feofan777@users.noreply.github.com>
2019-11-20 17:08:04 +0100

    working with Move for jpg

--- Model/SerialisationProcess.cs
+++ Model/SerialisationProcess.cs
@@ -12,7 +12,6 @@ namespace FotoSortIva02.Model
         public void DoSerialisation(Dictionary<string, string> _inMovePicDictionary)
         {
             // Serialisation -xml serialisation is not possible for dictionary - thats why i use SerialisationDictionary[] for serialisation
-            SerialisationDictionary serDictClass = new SerialisationDictionary();
             SerialisationDictionary[] arrayClassDict = new SerialisationDictionary[_inMovePicDictionary.Keys.Count()];
             int ii = 0;
             foreach (KeyValuePair<string, string> item in _inMovePicDictionary)
@@ -49,17 +48,17 @@ namespace FotoSortIva02.Model
             int numberItems = 0;
             Dictionary<string, string> readyDict = new Dictionary<string, string>();
 
-            XmlSerializer serializer = new XmlSerializer(typeof(SerialisationDictionary[]));
+            XmlSerializer serzer = new XmlSerializer(typeof(SerialisationDictionary[]));
             // получаем поток, куда будем записывать сериализованный объект
             using (FileStream fs = new FileStream("secretData.xml", FileMode.OpenOrCreate))
             {
 
                 try
                 {
-                  
-                    SerialisationDictionary[] arrayClassDict = (SerialisationDictionary[])serializer.Deserialize(fs);
-                    numberItems = arrayClassDict.Length;
-                    foreach (var ittem in arrayClassDict)
+                    
+                    SerialisationDictionary[] arrayClassDict1 = (SerialisationDictionary[])serzer.Deserialize(fs);
+                    numberItems = arrayClassDict1.Length;
+                    foreach (var ittem in arrayClassDict1)
                     {
                         readyDict.Add(ittem.keyy,ittem.valuee);
                     }
