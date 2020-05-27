08c8b528c5f48dc03091412ddf888a97c61ce0d8
IoanFirst <31549414+Feofan777@users.noreply.github.com>
2020-05-27 09:30:11 +0200

    Nothing, just test for GIT

---
+++ Model/neeeeew Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoSortIva02.Model
+{
+    class neeeeew_Class
+    {
+        public neeeeew_Class()
+        {
+            string sdf = "Hello";
+
+        }
+    }
+}
